ddb7053f25a7d22f55ad2de7b81be98345475e69
Rafael Quevedo <rafaelrpq@gmail.com>
2023-04-05 20:13:41 -0300

    ajustes moedeiro

--- cafeteira.php
+++ cafeteira.php
@@ -7,12 +7,12 @@
         ],
         'caixa' => 0,
         'moedas' => [
-            '0,01' => 0,
-            '0,05' => 0,
-            '0,10' => 0,
-            '0,25' => 0,
-            '0,50' => 0,
-            '1,00' => 0,
+            '0_01' => 0,
+            '0_05' => 0,
+            '0_10' => 0,
+            '0_25' => 0,
+            '0_50' => 0,
+            '1_00' => 0,
         ],
         'cedulas' => [
             '2' => 0,
@@ -34,6 +34,12 @@
         print (file_put_contents ('dados.json', json_encode ($cafeteira)));
     }
 
+    function adicionarMoedas ($moeda) {
+        global $cafeteira;
+        $cafeteira['moedas'][$moeda] += 1;
+        print (file_put_contents ('dados.json', json_encode ($cafeteira)));
+    }
+
     function iniciarCaixa ($valor) {
         global $cafeteira;
         $cafeteira['caixa'] = $valor;
@@ -45,10 +51,20 @@
         return $cafeteira['suprimentos'];
     }
 
+    function listaMoedas () {
+        global $cafeteira;
+        return $cafeteira['moedas'];
+    }
+
     switch ($_GET['data']) {
         case 'listar' :
             echo json_encode (listaSuprimentos());
             break;
+
+        case 'listarMoedas' :
+            echo json_encode (listaMoedas());
+            break;
+
         case 'adicionar' :
             switch ($_GET['sup']) {
                 case 'cafe' :
@@ -59,6 +75,27 @@
                     break;
                 case 'chocolate':
                     adicionarSuprimento ('chocolate');
+                    break;
+            }
+
+            switch ($_GET['moeda']) {
+                case '0_01':
+                    adicionarMoedas ('0_01');
+                    break;
+                case '0_05':
+                    adicionarMoedas ('0_05');
+                    break;
+                case '0_10':
+                    adicionarMoedas ('0_10');
+                    break;
+                case '0_25':
+                    adicionarMoedas ('0_25');
+                    break;
+                case '0_50':
+                    adicionarMoedas ('0_50');
+                    break;
+                case '1_00':
+                    adicionarMoedas ('1_00');
                     break;
             }
     }
---
+++ src/index.js
@@ -0,0 +1,4 @@
+document.querySelector ('form').addEventListener ('submit', e => {
+    e.preventDefault ()
+    console.log ('parou')
+})
